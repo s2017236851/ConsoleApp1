1ccb0760c448e13185caf3fbb95ae09c880f30ee
lichuang <lichuang@sayed.com.cn>
2024-03-13 11:57:18 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            for (int qq=0, i = 0; i < 4; i++,qq++)
+            {
+                Thread.Sleep(1000);
+                int aaa = i + qq;   
+                Console.Write(aaa);
+                a = i.ToString();
+            }
+            Console.WriteLine(a);
+        }
+        public string shujv() {
+            return "32323232";
+        }
+    }
+}
