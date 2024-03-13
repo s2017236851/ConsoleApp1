21db9a7695503115e7b352644d7545d461693340
lichuang <lichuang@sayed.com.cn>
2024-03-13 13:28:26 +0800

    shiyan

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,17 +12,27 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string a = "";
-            for (int qq=0, i = 0; i < 4; i++,qq++)
+            for (int qq = 0, i = 0; i < 4; i++, qq++)
             {
                 Thread.Sleep(1000);
-                int aaa = i + qq;   
+                int aaa = i + qq;
                 Console.Write(aaa);
                 a = i.ToString();
             }
             Console.WriteLine(a);
         }
-        public string shujv() {
+        public string shujv()
+        {
             return "32323232";
         }
+        public string qwqwq()
+        {
+            return "32323232";
+        }
+        public string qwqwqq()
+        {
+            return "32323232";
+        }
+
     }
 }
